96d3d77ba8097c532a14ac46af220a87abb5ac09
HP <HP@LAPTOP-RDR8DTM0.home-life.hub>
2022-11-14 16:01:28 +0100

    prima lezione

--- src/main/java/it/polito/tdp/ricorsione/model/Anagrammi.java
+++ src/main/java/it/polito/tdp/ricorsione/model/Anagrammi.java
@@ -1,43 +1,36 @@
 package it.polito.tdp.ricorsione.model;
 
+import it.polito.tdp.ricorsione.EntryPoint;
+
 public class Anagrammi {
-
-	/**
-	 * Riceve una stringa e stampa tutti i suoi anagrammi
-	 * 
-	 * @param s
-	 */
+	
+//Riceve una stringa e stampa tutti i suoi l'anagramma
 	public void anagramma(String s) {
-		anagramma_ricorsiva("", 0, s);
+		anagramma_ricorsiva("",0,s);
 	}
-
-	/**
-	 * Data una soluzione parziale, stampa TUTTI gli anagrammi che iniziano in quel
-	 * modo.
-	 * 
-	 * @param parziale  Soluzione parziale, iniziale, del mio anagramma. Avrà
-	 *                  'livello' caratteri.
-	 * @param livello   Livello della ricorsione
-	 * @param rimanenti I caratteri ancora da sistemare
-	 */
+	
+//Data una soluzione parziale stampa turti gli anagrammi che iniziano con le lettere già fissate
+	//@param parziale = Soluzione parziale,iniziale, del mio anagramma
+	//@param livello = livello dellla ricorsione
+	//@param rimanenti = caratteri ancora da sistemare
 	private void anagramma_ricorsiva(String parziale, int livello, String rimanenti) {
-		if (rimanenti.length() == 0) { /* caso terminale */
+		if(rimanenti.length()==0) {/*caso terminale*/
 			System.out.println(parziale);
-		} else {
-			/* caso normale */
-			// es: parziale="AC", livello=2, rimanenti="BD"
-			for (int pos = 0; pos < rimanenti.length(); pos++) {
-				String nuova_parziale = parziale + rimanenti.charAt(pos);
-				String nuova_rimanenti = rimanenti.substring(0, pos) + rimanenti.substring(pos + 1);
-				anagramma_ricorsiva(nuova_parziale, livello + 1, nuova_rimanenti);
+		}else {
+			/*caso normale*/
+			//es:parziale='AC', livello=2, rimanenti='BD'
+			for(int pos=0; pos<rimanenti.length(); pos++) {
+				String nuova_parziale=parziale+rimanenti.charAt(pos);
+				String nuova_rimanenti= rimanenti.substring(0, pos)+rimanenti.substring(pos+1);
+				anagramma_ricorsiva(nuova_parziale, livello+1, nuova_rimanenti);
 			}
+			
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Anagrammi main = new Anagrammi();
+		Anagrammi main= new Anagrammi();
 		main.anagramma("ABCD");
-	}
-
+     }
+	
 }
